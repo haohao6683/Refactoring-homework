513bb188bac9badfa65bdd55dc4687dbf416c7c2
haohao6683 <852721803@qq.com>
2020-08-28 02:03:27 +0800

    [RankTest][Draymond] test: add test 'ranking case 2 when voyage.zone is china'.

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,7 +1,7 @@
 const rankTest = require('ava');
 const {rating} = require('../src/rank');
 
-rankTest('ranking case default', t => {
+rankTest('ranking case 1 when voyage.zone is west-indies', t => {
   const voyage = {
     zone: 'west-indies',
     length: 10,
@@ -27,4 +27,32 @@ rankTest('ranking case default', t => {
   const result = rating (voyage, history);
 
   t.is(result, 'B');
+});
+
+rankTest('ranking case 2 when voyage.zone is china', t => {
+  const voyage = {
+    zone: 'china',
+    length: 10,
+  };
+
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+  ];
+
+  const result = rating (voyage, history);
+
+  t.is(result, 'A');
 });
