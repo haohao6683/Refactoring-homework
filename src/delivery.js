19a081cd2c78bd0d92ddcc2e13b642f5c455af2e
haohao6683 <852721803@qq.com>
2020-08-29 00:25:55 +0800

    [RankTest][Draymond] Refactor: refactor method deliveryDate().

--- src/delivery.js
+++ src/delivery.js
@@ -1,43 +1,17 @@
 function deliveryDate (anOrder, isRush) {
+  let deliveryTime;
+  let value;
   if (isRush) {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
-    }
-    else if ([
-      'NY',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else {
-      deliveryTime = 3;
-    }
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
+    value = 1;
+    deliveryTime = (['MA','CT',].includes(anOrder.deliveryState)) ? 1
+                            : (['NY','NH',].includes(anOrder.deliveryState)) ? 2 : 3;
   }
   else {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-      'NY',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else if ([
-      'ME',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    }
-    else {
-      deliveryTime = 4;
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+    value = 2;
+    deliveryTime = (['MA','CT','NY',].includes(anOrder.deliveryState)) ? 2
+                            : (['ME','NH',].includes(anOrder.deliveryState)) ? 3 : 4;
   }
+  return anOrder.placedOn.plusDays(value + deliveryTime);
 }
 
 module.exports = {
