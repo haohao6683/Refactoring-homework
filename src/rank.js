7978e9d98c3af6821defd6fb9aafcf3f9721dd92
haohao6683 <852721803@qq.com>
2020-08-28 23:34:42 +0800

    `[RankTest][Draymond] Refactor: refactor method voyageProfitFactor() to replace the if-else statement by "? :".

--- src/rank.js
+++ src/rank.js
@@ -20,28 +20,16 @@ function captainHistoryRisk (voyage, history) {
 
 function voyageProfitFactor (voyage, history) {
   let result = 2;
-  if (voyage.zone === 'china' || voyage.zone === 'east-indies') {
-    result += 1;
-  }
+  result += (voyage.zone === 'china' || voyage.zone === 'east-indies') ? 1 : 0;
   if (voyage.zone === 'china' && hasChina(history)) {
     result += 3;
-    if (history.length > 10) {
-      result += 1;
-    }
-    if (voyage.length > 12) {
-      result += 1;
-    }
-    if (voyage.length > 18) {
-      result -= 1;
-    }
+    result += (history.length > 10) ? 1 : 0;
+    result += (voyage.length > 12) ? 1 : 0;
+    result -= (voyage.length > 18) ? 1 : 0;
   }
   else {
-    if (history.length > 8) {
-      result += 1;
-    }
-    if (voyage.length > 14) {
-      result -= 1;
-    }
+    result += (history.length > 8) ? 1 : 0;
+    result -= (voyage.length > 14) ? 1 : 0;
   }
   return result;
 }
