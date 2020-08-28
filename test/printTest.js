1dbfcc61e00ed04a625d8db7cc9badb698abeb11
haohao6683 <852721803@qq.com>
2020-08-29 01:01:59 +0800

    [RankTest][Draymond] test: add another test-case in printTest.js.

--- test/printTest.js
+++ test/printTest.js
@@ -1,7 +1,7 @@
 const printTest = require('ava');
 const {printOwing} = require('../src/print');
 
-printTest('test case 1 when printOwing', t => {
+printTest('test case 1 when printOwing given invoice.name has nothing', t => {
     let invoice = {
         borderSpacing: [
             {amount: 1},
@@ -15,6 +15,25 @@ printTest('test case 1 when printOwing', t => {
                   '**** Customer Owes ****' +
                   '***********************' +
                   'name: undefined' +
+                  'amount: 3' +
+                  `amount: ${dueDate.toLocaleDateString()}`);
+})
+
+printTest('test case 2 when printOwing given invoice has name Draymond.', t => {
+    let invoice = {
+        customer: 'Draymond',
+        borderSpacing: [
+            {amount: 1},
+            {amount: 2}
+        ]
+    }
+    const result = printOwing(invoice);
+    const today = new Date();
+    const dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    t.is(result, '***********************' +
+                  '**** Customer Owes ****' +
+                  '***********************' +
+                  'name: Draymond' +
                   'amount: 3' +
                   `amount: ${dueDate.toLocaleDateString()}`);
 })
